cce099d44cb22523ab9ccf9a129ead235d47565e
Amadou Touré <131898454+amadoutoure14@users.noreply.github.com>
2025-07-09 11:44:18 +0000

    champ d'immatriculation

--- src/app/component/ajouter-vehicule/ajouter-vehicule.component.ts
+++ src/app/component/ajouter-vehicule/ajouter-vehicule.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {afterRender, afterRenderEffect, Component, ElementRef, inject, OnInit, ViewChild} from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import {VehiculeService} from '../../services/vehicule.service';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
@@ -16,19 +16,64 @@ import {NgIf} from '@angular/common';
 })
 export class AjouterVehiculeComponent implements OnInit {
   vehiculeForm!: FormGroup;
-
-  constructor(
-    private fb: FormBuilder,
-    private service: VehiculeService,
-    private snackBar: MatSnackBar
-  ) {}
+  private isFormatting = false;
+  constructor(private fb: FormBuilder, private service: VehiculeService, private snackBar: MatSnackBar) {
+    const elementRef = inject(ElementRef);
+    elementRef.nativeElement.querySelector('immatrucilation')?.setStyle('color', 'red');
+  }
 
   ngOnInit(): void {
     this.vehiculeForm = this.fb.group({
-      immatriculation: ['', Validators.required],
+      immatriculation: [
+        '',
+        Validators.required,
+      ],
       modele: ['', Validators.required],
       commentaire: ['']
     });
+  }
+  formatImmatriculation(event: Event): void {
+    if (this.isFormatting) return;
+
+    const input = event.target as HTMLInputElement;
+    const rawValue = input.value.toUpperCase().replace(/[^A-Z0-9]/g, '');
+    const previousValue = this.vehiculeForm.get('immatriculation')?.value || '';
+
+    // Si l'utilisateur supprime tout, ne pas reformater
+    if (rawValue.length <= 2) return;
+
+    let formatted = '';
+    if (rawValue.length <= 7) {
+      // Format AB 123 CD
+      let AA = rawValue.substring(0, 2);
+      let BB = rawValue.substring(2, 5);
+      let CC = rawValue.substring(5, 7);
+
+      formatted = AA;
+      if (BB) formatted += ' ' + BB;
+      if (CC) formatted += ' ' + CC;
+    } else {
+      // Format AB 1234 CD
+      let AA = rawValue.substring(0, 2);
+      let BB = rawValue.substring(2, 6);
+      let CC = rawValue.substring(6, 8);
+
+      formatted = AA;
+      if (BB) formatted += ' ' + BB;
+      if (CC) formatted += ' ' + CC;
+    }
+
+    // Appliquer le format seulement si différent pour éviter blocage
+    if (formatted !== previousValue) {
+      this.isFormatting = true;
+      this.vehiculeForm.get('immatriculation')?.setValue(formatted, { emitEvent: false });
+
+      // Repositionner le curseur en fin
+      setTimeout(() => {
+        input.selectionStart = input.selectionEnd = formatted.length;
+        this.isFormatting = false;
+      });
+    }
   }
 
   onSubmit(): void {
@@ -44,4 +89,7 @@ export class AjouterVehiculeComponent implements OnInit {
       });
     }
   }
+
+
+
 }
